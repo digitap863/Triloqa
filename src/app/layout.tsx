759ecb73ab633efd7a514b9a0dc26f211023e5d5
Aginas pk <aginaspk6@gmail.com>
2026-03-16 17:42:18 +0530

    overflow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,9 +42,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="overflow-x-hidden">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${montserratAlternates.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${montserratAlternates.variable} antialiased overflow-x-hidden`}
       >
         <AosInit />
         {children}
