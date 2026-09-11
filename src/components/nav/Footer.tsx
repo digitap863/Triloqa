34710af9b2d5ea58b41a8f70d4d5c6b9403a8e31
Aginas pk <aginaspk6@gmail.com>
2026-09-11 16:39:29 +0530

    footer

--- src/components/nav/Footer.tsx
+++ src/components/nav/Footer.tsx
@@ -202,7 +202,7 @@ const Footer = () => {
             {/* COPYRIGHT */}
             <div className="border-t border-white/5 bg-[#23232C]">
                 <div className="max-w-7xl mx-auto px-6 py-6 flex flex-col md:row items-center justify-between gap-4 text-sm text-white/50">
-                    <p>© {new Date().getFullYear()} <span className="text-[#1D8F2C] font-bold">Tapclone</span>. All Rights Reserved.</p>
+                    <Link href={'https://www.tapclone.in/'}>© {new Date().getFullYear()} <span className="text-[#1D8F2C] font-bold">Tapclone</span>. All Rights Reserved.</Link>
                     <div className="flex gap-6">
                         <Link href="#" className="hover:text-white transition-colors">Privacy Policy</Link>
                         <Link href="#" className="hover:text-white transition-colors">Terms of Service</Link>
