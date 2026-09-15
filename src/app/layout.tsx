2377c4df5d18505d6753e4754e1bfb0c37ee252e
muhammed  arshaq tk <arshaqtk4@gmail.com>
2026-09-15 15:28:07 +0530

    feat: add root layout with custom fonts, analytics, and Tiptap dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Montserrat_Alternates } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { AosInit } from "@/components/AosInit";
 
@@ -45,6 +46,17 @@ export default function RootLayout({
   return (
     <html lang="en" className="overflow-x-hidden">
       <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M7GXLCCG');`,
+          }}
+        />
         <meta
           name="google-site-verification"
           content="krEJUK030Rrgf3_xNMwi7oBMuXMUHEpI7NJs78uPI38"
@@ -53,6 +65,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${montserratAlternates.variable} antialiased overflow-x-hidden`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M7GXLCCG"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <AosInit />
         {children}
       </body>
